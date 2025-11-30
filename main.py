ca657acd66807d3cc9fe6e64aa6825476ef945ec
Irfan Islam <patweryif@gmail.com>
2025-11-30 19:41:57 +0300

    Changes to code to accomodate hatem's part along with the names of person responsible for their part

--- main.py
+++ main.py
@@ -1,34 +1,43 @@
-import BankersAlgorithm
-import part3
+import cpu_schedule  # Hatem
+import BankersAlgorithm  # Mohammad and Ahmad
+import part3  # Irfan
+
 
 def show_menu():
     while True:
         print("          OS Simulator         ")
         print("=" * 30)
-        print("[1] Hatem's Part")
+        print("[1] CPU Scheduling")
         print("[2] Banker's Algorithm")
         print("[3] Page Replacement Algorithms")
         print("[0] Exit")
 
         choice = input("Enter your choice: ").strip()
+
         if choice == "1":
-          print("\nHatem's part")
-          input("\nPress Enter to return to the menu...")
+            print("\n--- CPU Scheduling ---")
+            cpu_schedule.run()
+            input("\nPress Enter to return to the menu...")
+
         elif choice == "2":
+            print("\n--- Banker's Algorithm ---")
             algo = BankersAlgorithm.BankersAlgorithm()
             algo.run()
             input("\nPress Enter to return to the menu...")
+
         elif choice == "3":
-            print("\n--- Page Replacement Algorithms (Part 3) ---")
+            print("\n--- Page Replacement Algorithms ---")
             part3.run()
             input("\nPress Enter to return to the menu...")
 
         elif choice == "0":
             print("\nExiting program...")
             break
+
         else:
             print("\nInvalid input, try again.")
-            input("Press Enter...")
+            input("Press Enter to continue...")
+
 
 if __name__ == "__main__":
     show_menu()
